72ddb1ab751bb7f28207e4400af125ff567943f7
beelarr <bryonl@me.com>
2018-10-03 19:24:41 -0500

    Move data to store with vuex

---
+++ data/stocks.ts
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, name: 'BMW', price: 110},
+    {id: 2, name: 'Google', price: 10},
+    {id: 3, name: 'Apple', price: 130},
+    {id: 4, name: 'Yo Mama', price: 1}
+];
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,24 +1,19 @@
 <template>
     <div>
-        <app-stock v-for="stock in stocks"></app-stock>
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
     </div>
 </template>
 <script>
     import Stock from './Stock.vue';
 
     export default {
-         data() {
-             return {
-                 stocks: [
-                     {id: 1, name: 'BMW', price: 110},
-                     {id: 2, name: 'Google', price: 10},
-                     {id: 3, name: 'Apple', price: 130},
-                     {id: 4, name: 'Yo Mama', price: 1}
-                 ]
-             }
-         },
         components: {
             appStock: Stock
+        },
+        computed: {
+            stocks() {
+                return this.$store.getters.stocks;
+            }
         }
     }
 </script>
